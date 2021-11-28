eeea109baeac142cd9173d1adfe470147783270c
TiphVampHeartblood <tiphaine.riallot@gmail.com>
2021-11-28 21:48:46 +0100

    Test Ecran de Fin pas concluant

---
+++ New Unity Project/Assets/Scripts/menu/EndMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class EndMenu : MonoBehaviour
+{
+    public string level;
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitButton()
+    {
+        EditorApplication.ExitPlaymode();
+        Application.Quit();
+    }
+}
