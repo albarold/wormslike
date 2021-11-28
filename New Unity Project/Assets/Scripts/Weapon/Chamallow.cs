c1a6cdebc865cb5d047a91b577b9c0ef8b7e2798
albarold <alan.coeu@gmail.com>
2021-11-28 21:20:42 +0100

    chamallow size

--- New Unity Project/Assets/Scripts/Weapon/Chamallow.cs
+++ New Unity Project/Assets/Scripts/Weapon/Chamallow.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Chamallow : MonoBehaviour
 {
+    public Vector3 ScaleChange;
     private int CollideNb = 0;
     public int DegatSupp = 0;
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 3)//ground
         {
+            gameObject.transform.localScale += ScaleChange;
             GetComponent<DoDamage>()._Damages+=DegatSupp;
             CollideNb++;
             if (CollideNb >= 3)
             {
+                
                 Destroy(gameObject);
             }
         }
